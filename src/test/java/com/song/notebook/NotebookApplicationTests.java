990ea95edbb52d5159727d03409704699116264c
Song Yang <tianhuilin45@gmail.com>
2024-11-15 23:02:21 +0000

    Initialize localhost setup with JPA, Thymeleaf, and Spring Boot

---
+++ src/test/java/com/song/notebook/NotebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.song.notebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
